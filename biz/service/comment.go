a993f017287f95c0f29c7d4dd670704d685015bd
DawnSiro <1220383372@qq.com>
2023-03-02 17:12:28 +0800

    进行了全面测试，修复了评论区缓存和查看别人的关注列表关注状态不对的bug

--- biz/service/comment.go
+++ biz/service/comment.go
@@ -1,10 +1,6 @@
 package service
 
 import (
-	"douyin/pkg/global"
-	"strconv"
-	"strings"
-
 	"douyin/biz/model/api"
 	"douyin/dal/db"
 	"douyin/dal/pack"
@@ -12,18 +8,16 @@ import (
 	"douyin/pkg/util/sensitive"
 
 	"github.com/cloudwego/hertz/pkg/common/hlog"
-	"github.com/cloudwego/hertz/pkg/common/json"
-	"github.com/go-redis/redis"
 )
 
 func PostComment(userID, videoID uint64, commentText string) (*api.DouyinCommentActionResponse, error) {
 	// 删除redis评论列表缓存
 	// 使用 strings.Builder 来优化字符串的拼接
-	var builder strings.Builder
-	builder.WriteString(strconv.FormatUint(videoID, 10))
-	builder.WriteString("_video_comments")
-	delCommentListKey := builder.String()
-	hlog.Info("service.comment.PostComment delCommentListKey:", delCommentListKey)
+	//var builder strings.Builder
+	//builder.WriteString(strconv.FormatUint(videoID, 10))
+	//builder.WriteString("_video_comments")
+	//delCommentListKey := builder.String()
+	//hlog.Info("service.comment.PostComment delCommentListKey:", delCommentListKey)
 
 	//检测是否带有敏感词
 	if sensitive.IsWordsFilter(commentText) {
@@ -85,51 +79,51 @@ func DeleteComment(userID, videoID, commentID uint64) (*api.DouyinCommentActionR
 }
 
 func GetCommentList(userID, videoID uint64) (*api.DouyinCommentListResponse, error) {
-	var builder strings.Builder
-	builder.WriteString(strconv.FormatUint(userID, 10))
-	builder.WriteString("_userId_")
-	builder.WriteString(strconv.FormatUint(videoID, 10))
-	builder.WriteString("_video_comments")
-	commentListKey := builder.String()
+	//var builder strings.Builder
+	//builder.WriteString(strconv.FormatUint(userID, 10))
+	//builder.WriteString("_userId_")
+	//builder.WriteString(strconv.FormatUint(videoID, 10))
+	//builder.WriteString("_video_comments")
+	//commentListKey := builder.String()
 
-	commentList, err := global.VideoCRC.Get(commentListKey).Result()
-	if err == redis.Nil {
-		dbcList, err := db.SelectCommentListByVideoID(videoID)
-		if err != nil {
-			hlog.Error("service.comment.GetCommentList err:", err.Error())
-			return nil, err
-		}
-
-		cList := make([]*api.Comment, 0, len(dbcList))
-
-		for i := 0; i < len(dbcList); i++ {
-			u, _ := db.SelectUserByID(dbcList[i].UserID)
-			cList = append(cList, pack.Comment(dbcList[i], u, db.IsFollow(userID, dbcList[i].UserID)))
-		}
-
-		//序列化
-		marshalList, _ := json.Marshal(cList)
-		_, err = global.VideoCRC.Set(commentListKey, marshalList, 0).Result()
-		if err != nil {
-			hlog.Error("service.comment.GetCommentList err:", err.Error())
-			return nil, err
-		}
-		commentList, err = global.VideoCRC.Get(commentListKey).Result()
-		if err != nil {
-			hlog.Error("service.comment.GetCommentList err:", err.Error())
-			return nil, err
-		}
-	}
-	//反序列化
-	var list []*api.Comment
-	err = json.Unmarshal([]byte(commentList), &list)
+	//commentList, err := global.VideoCRC.Get(commentListKey).Result()
+	//if err == redis.Nil {
+	dbcList, err := db.SelectCommentListByVideoID(videoID)
 	if err != nil {
 		hlog.Error("service.comment.GetCommentList err:", err.Error())
 		return nil, err
 	}
 
+	cList := make([]*api.Comment, 0, len(dbcList))
+
+	for i := 0; i < len(dbcList); i++ {
+		u, _ := db.SelectUserByID(dbcList[i].UserID)
+		cList = append(cList, pack.Comment(dbcList[i], u, db.IsFollow(userID, dbcList[i].UserID)))
+	}
+
+	//序列化
+	//marshalList, _ := json.Marshal(cList)
+	//_, err = global.VideoCRC.Set(commentListKey, marshalList, 0).Result()
+	//if err != nil {
+	//	hlog.Error("service.comment.GetCommentList err:", err.Error())
+	//	return nil, err
+	//}
+	//commentList, err = global.VideoCRC.Get(commentListKey).Result()
+	//if err != nil {
+	//	hlog.Error("service.comment.GetCommentList err:", err.Error())
+	//	return nil, err
+	//}
+	//}
+	//反序列化
+	//var list []*api.Comment
+	//err = json.Unmarshal([]byte(commentList), &list)
+	//if err != nil {
+	//	hlog.Error("service.comment.GetCommentList err:", err.Error())
+	//	return nil, err
+	//}
+
 	return &api.DouyinCommentListResponse{
 		StatusCode:  0,
-		CommentList: list,
+		CommentList: cList,
 	}, nil
 }
