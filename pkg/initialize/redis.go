9f5ba47c6b97deb0168fc50e9a40361f20e108da
yh342 <yh342@duke.edu>
2023-09-04 10:50:47 -0400

    !Problem with updating cache

--- pkg/initialize/redis.go
+++ pkg/initialize/redis.go
@@ -88,8 +88,8 @@ func Redis() {
 				hlog.Error("WarmUpCacheForTopComments error: " + err.Error())
 			}
 
-			// Wait for 7 days before the next warm-up
-			time.Sleep(7 * 24 * time.Hour)
+			// Wait for 15 minutes before the next warm-up
+			time.Sleep(15 * time.Minute)
 		}
 	}()
 
@@ -125,25 +125,15 @@ func SubscribeToTopicChanges() {
 					hlog.Error("pkg.initialize.redis.SubscribeToChanges err: " + err.Error())
 				}
 				parts := strings.Split(msg.Payload, "&")
+				ID1, _ := strconv.ParseUint(parts[1], 10, 64)
+				ID2, _ := strconv.ParseUint(parts[2], 10, 64)
 
-				if parts[0] == "friend_followed" || parts[0] == "friend_unfollowed" {
-					// Invalidate the cached friend list
-					friendListCacheKeyFromUser := "friendList:" + parts[1]
-					friendListCacheKeyToUser := "friendList:" + parts[2]
-					followerListCacheKeyFromUser := "followerList:" + parts[1]
-					followerListCacheKeyToUser := "followerList:" + parts[2]
-					followListCacheKeyFromUser := "followList:" + parts[1]
-					followListCacheKeyToUser := "followList:" + parts[2]
-					err := global.UserInfoRC.Del(friendListCacheKeyFromUser).Err()
-					err = global.UserInfoRC.Del(friendListCacheKeyToUser).Err()
-					err = global.UserInfoRC.Del(followerListCacheKeyFromUser).Err()
-					err = global.UserInfoRC.Del(followerListCacheKeyToUser).Err()
-					err = global.UserInfoRC.Del(followListCacheKeyFromUser).Err()
-					err = global.UserInfoRC.Del(followListCacheKeyToUser).Err()
-					if err != nil {
-						hlog.Error("pkg.initialize.redis.SubscribeToTopicChanges.relation err: ", err.Error())
-					}
-				} else if parts[0] == "comment_added" || parts[0] == "comment_deleted" {
+				switch parts[0] {
+				case "friend_followed":
+					updateUserRelationships(ID1, ID2, true)
+				case "friend_unfollowed":
+					updateUserRelationships(ID1, ID2, false)
+				case "comment_added", "comment_deleted":
 					// Invalidate the cache for comment
 					videoComment := "commentList:" + parts[1] + ":" + parts[2]
 					err := global.UserInfoRC.Del(videoComment).Err()
@@ -156,6 +146,170 @@ func SubscribeToTopicChanges() {
 	}
 
 	wg.Wait()
+}
+
+func updateUserRelationships(followerID, followedID uint64, follow bool) {
+	updateFollowerList(followerID, followedID, follow)
+
+	updateFollowList(followerID, followedID, follow)
+
+	updateFriendLists(followerID, followedID, follow)
+}
+
+func updateFollowerList(userID, followedID uint64, follow bool) {
+	// Update the follower list for followedID
+
+	followerListCacheKeyToUser := fmt.Sprintf("followList:%d", followedID)
+
+	// Get the current follower list from Redis
+	currentFollowerListJSON, err := global.UserInfoRC.Get(followerListCacheKeyToUser).Result()
+	if err != nil {
+		hlog.Error("pkg.initialize.redis.SubscribeToTopicChanges.relation err: ", err.Error())
+		return
+	}
+
+	var currentFollowerList []*db.RelationUserData
+	if err := json.Unmarshal([]byte(currentFollowerListJSON), &currentFollowerList); err != nil {
+		hlog.Error("pkg.initialize.redis.SubscribeToTopicChanges.relation err: Error decoding follower list, ", err.Error())
+		return
+	}
+
+	if follow {
+		// If follow is true, add the user if not found in the list
+		// TODO:这里需要构造结构体
+		currentUserData := &db.RelationUserData{UID: userID}
+		currentFollowerList = append(currentFollowerList, currentUserData)
+	} else {
+		// If follow is false, remove the user from the list if found
+		updatedFollowerList := make([]*db.RelationUserData, 0, len(currentFollowerList)-1)
+		for _, user := range currentFollowerList {
+			if user.UID != userID {
+				updatedFollowerList = append(updatedFollowerList, user)
+			}
+		}
+		currentFollowerList = updatedFollowerList
+
+	}
+
+	// Update the follower list in Redis
+	updatedFollowerListJSON, _ := json.Marshal(currentFollowerList)
+	err = global.UserInfoRC.Set(followerListCacheKeyToUser, updatedFollowerListJSON, 0).Err()
+	if err != nil {
+		hlog.Error("pkg.initialize.redis.SubscribeToTopicChanges.relation err: Error updating follower list, ", err.Error())
+	}
+}
+
+func updateFollowList(userID, followedID uint64, follow bool) {
+	// Update the follow list for userID
+
+	followListCacheKey := fmt.Sprintf("followList:%d", userID)
+
+	// Get the current follow list from Redis
+	currentFollowListJSON, err := global.UserInfoRC.Get(followListCacheKey).Result()
+	if err != nil {
+		hlog.Error("pkg.initialize.redis.SubscribeToTopicChanges.relation err: ", err.Error())
+		return
+	}
+
+	var currentFollowList []*db.RelationUserData
+	if err := json.Unmarshal([]byte(currentFollowListJSON), &currentFollowList); err != nil {
+		hlog.Error("pkg.initialize.redis.SubscribeToTopicChanges.relation err: Error decoding follow list, ", err.Error())
+		return
+	}
+
+	if follow {
+		// If follow is true, add the followed user if not found in the list
+		// TODO:这里需要构造结构体
+		followedUserData := &db.RelationUserData{UID: followedID}
+		currentFollowList = append(currentFollowList, followedUserData)
+	} else {
+		// If follow is false, remove the followed user from the list if found
+		updatedFollowList := make([]*db.RelationUserData, 0, len(currentFollowList)-1)
+		for _, user := range currentFollowList {
+			if user.UID != followedID {
+				updatedFollowList = append(updatedFollowList, user)
+			}
+		}
+		currentFollowList = updatedFollowList
+	}
+
+	// Update the follow list in Redis
+	updatedFollowListJSON, _ := json.Marshal(currentFollowList)
+	err = global.UserInfoRC.Set(followListCacheKey, updatedFollowListJSON, 0).Err()
+	if err != nil {
+		hlog.Error("pkg.initialize.redis.SubscribeToTopicChanges.relation err: Error updating follow list, ", err.Error())
+	}
+}
+
+func updateFriendLists(user1ID, user2ID uint64, follow bool) {
+	// Update friend list for user1 (user1 follows user2)
+	updateFriendList(user1ID, user2ID, follow)
+
+	// Update friend list for user2 (user2 follows user1)
+	updateFriendList(user2ID, user1ID, follow)
+}
+
+func updateFriendList(userID, friendID uint64, follow bool) {
+	// Update the friend list for userID
+
+	friendListCacheKey := fmt.Sprintf("friendList:%d", userID)
+
+	// Get the current friend list from Redis
+	currentFriendListJSON, err := global.UserInfoRC.Get(friendListCacheKey).Result()
+	if err != nil {
+		hlog.Error("pkg.initialize.redis.SubscribeToTopicChanges.relation err: ", err.Error())
+		return
+	}
+
+	var currentFriendList []*api.FriendUser
+	if err := json.Unmarshal([]byte(currentFriendListJSON), &currentFriendList); err != nil {
+		hlog.Error("pkg.initialize.redis.SubscribeToTopicChanges.relation err: Error decoding friend list, ", err.Error())
+		return
+	}
+
+	// Find the friend in the current friend list, or add them if not found
+	friendFound := false
+	for _, friend := range currentFriendList {
+		if uint64(friend.ID) == friendID {
+			friendFound = true
+			break
+		}
+	}
+
+	if follow {
+		// If follow is true, add the friend if not found in the list
+		if !friendFound {
+			// TODO:
+			userList, _ := db.GetFriendList(userID)
+			friendUserList := make([]*api.FriendUser, 0, len(userList))
+			for _, u := range userList {
+				msg, err := db.GetLatestMsg(userID, u.ID)
+				if err != nil {
+					hlog.Error("pkg.initialize.redis.SubscribeToTopicChanges.relation err: ", err.Error())
+					return
+				}
+				friendUserList = append(friendUserList, pack.FriendUser(u, db.IsFollow(userID, u.ID), msg.Content, msg.MsgType))
+			}
+		}
+	} else {
+		// If follow is false, remove the friend from the list if found
+		if friendFound {
+			updatedFriendList := make([]*api.FriendUser, 0, len(currentFriendList)-1)
+			for _, friend := range currentFriendList {
+				if uint64(friend.ID) != friendID {
+					updatedFriendList = append(updatedFriendList, friend)
+				}
+			}
+			currentFriendList = updatedFriendList
+		}
+	}
+
+	// Update the friend list in Redis
+	updatedFriendListJSON, _ := json.Marshal(currentFriendList)
+	err = global.UserInfoRC.Set(friendListCacheKey, updatedFriendListJSON, 0).Err()
+	if err != nil {
+		hlog.Error("pkg.initialize.redis.SubscribeToTopicChanges.relation err: Error updating friend list, ", err.Error())
+	}
 }
 
 func WarmUpCacheForTopFollowers() error {
@@ -276,7 +430,7 @@ func WarmUpCacheForTopComments() error {
 		responseJSON, _ := json.Marshal(response)
 
 		// Set cache expiration with some randomization to prevent cache stampede
-		cacheDuration := 10*time.Minute + time.Duration(rand.Intn(600))*time.Second
+		cacheDuration := 10*time.Hour + time.Duration(rand.Intn(10))*time.Hour
 
 		err = global.UserInfoRC.Set(cacheKey, responseJSON, cacheDuration).Err()
 		if err != nil {
